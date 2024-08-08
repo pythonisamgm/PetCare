e052836d939ac7b9c41431de14176ff628635985
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-08-08 14:25:09 +0200

    feat: add methods for get by name, by mail, update and delete

--- src/main/java/com/example/PetCare/controllers/GuardianController.java
+++ src/main/java/com/example/PetCare/controllers/GuardianController.java
@@ -6,6 +6,8 @@ import com.example.PetCare.dto.appointment.PostAppointmentDTO;
 import com.example.PetCare.dto.guardian.GuardianConverter;
 import com.example.PetCare.dto.guardian.GuardianDTO;
 import com.example.PetCare.dto.guardian.PostGuardianDTO;
+import com.example.PetCare.dto.pet.PetConverter;
+import com.example.PetCare.dto.pet.PetDTO;
 import com.example.PetCare.models.Appointment;
 import com.example.PetCare.models.Guardian;
 import com.example.PetCare.services.AppointmentServiceImpl;
@@ -40,16 +42,20 @@ public class GuardianController {
     @Autowired
     private GuardianConverter guardianConverter;
 
+    @Autowired
+    private PetConverter petConverter;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public GuardianDTO createGuardian(@RequestBody PostGuardianDTO postGuardianDTO) {
-        logger.info("POST /api/v1/appointments --> " + postGuardianDTO.toString());
+        logger.info("POST /api/v1/guardians --> " + postGuardianDTO.toString());
         Guardian guardian = guardianConverter.postDtoToGuardian(postGuardianDTO);
         Guardian createdGuardian = guardianService.createGuardian(guardian);
         return guardianConverter.guardianToDto(createdGuardian);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<GuardianDTO> getAllGuardians() {
         logger.info("GET /api/v1/guardians");
         List<Guardian> guardians = guardianService.getAllGuardians();
@@ -57,21 +63,61 @@ public class GuardianController {
                 .map(guardianConverter::guardianToDto)
                 .collect(Collectors.toList());
     }
+
     @GetMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Optional<GuardianDTO> getByGuardiansById(@PathVariable Long id) {
+    public Optional<GuardianDTO> getGuardianById(@PathVariable Long id) {
+        logger.info("GET /api/v1/guardians" + id);
         Optional<Guardian> guardian = guardianService.getGuardianById(id);
         return guardian.map(guardianConverter::guardianToDto);
     }
 
-    @GetMapping(path="{name}")
+    @GetMapping(path = "{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<GuardianDTO> getGuardianByName(@RequestParam String guardianName) {
+        logger.info("GET /api/v1/guardians/name?guardianName=" + guardianName);
+        return guardianService.getGuardianByName(guardianName)
+                .map(guardian -> {
+                    PetDTO petDTO = petConverter.entityToDTO(guardian.getPet());
+                    return GuardianDTO.builder()
+                            .guardianId(guardian.getGuardianId())
+                            .guardianName(guardian.getGuardianName())
+                            .telephoneNumber(guardian.getTelephoneNumber())
+                            .email(guardian.getEmail())
+                            .sysRegistryDate(guardian.getSysRegistryDate())
+                            .pet(petDTO)
+                            .build();
+                });
+    }
+    @GetMapping(path = "{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<GuardianDTO> getGuardianByEmail(@RequestParam String email) {
+        logger.info("GET /api/v1/guardians/email?email=" + email);
+        return guardianService.getGuardianByMail(email)
+                .map(guardian -> {
+                    PetDTO petDTO = petConverter.entityToDTO(guardian.getPet());
+                    return GuardianDTO.builder()
+                            .guardianId(guardian.getGuardianId())
+                            .guardianName(guardian.getGuardianName())
+                            .telephoneNumber(guardian.getTelephoneNumber())
+                            .email(guardian.getEmail())
+                            .sysRegistryDate(guardian.getSysRegistryDate())
+                            .pet(petDTO)
+                            .build();
+                });
+    }
 
     @PutMapping(path = "/{id}")
-    public Guardian updateGuardian(@RequestBody Guardian guardian, @PathVariable Long id) throws Exception {
-        return guardianService.updateGuardian(id, guardian);
+    public void updateGuardian(@RequestBody PostGuardianDTO postGuardianDTO, @PathVariable Long id) throws Exception {
+        logger.info("PUT /api/v1/guardians/{} --> {}", id, postGuardianDTO.toString());
+        Guardian request = guardianConverter.postDtoToGuardian(postGuardianDTO);
+        guardianService.updateGuardian(id, request);
     }
+
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteGuardian(@PathVariable Long id) {
+        logger.info("DELETE /api/v1/guardians/{}", id);
         guardianService.deleteGuardian(id);
     }
 
