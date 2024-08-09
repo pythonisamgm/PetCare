62141aa276426d3f55055a187956db9dd29be346
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-08-09 21:31:11 +0200

    fix: create function didnt have another in service

--- src/main/java/com/example/PetCare/services/interfaces/GuardianService.java
+++ src/main/java/com/example/PetCare/services/interfaces/GuardianService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public interface GuardianService {
+    public Guardian createGuardian(Guardian guardian) throws Exception;
     public Optional<Guardian> getGuardianById(Long id);
     public ArrayList<Guardian> getAllGuardians();
     public Guardian updateGuardian(Long id, Guardian newGuardian) throws Exception;
